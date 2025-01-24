a489b9bbbe24150b2d76a8ce439450231aa806ac
HP <samu.kauppi@metropolia.fi>
2025-01-24 22:34:05 +0200

    add cat movement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,23 +7,25 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private Transform gravityPoint;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 10f;
-    [SerializeField] private float angleOffset = 10f;
-
-    private float deltaAngle;
+    [SerializeField] private float maxSpeed = 10f;
 
     private void FixedUpdate()
     {
         transform.up = gravityPoint.position - transform.position;
+        rb.rotation = transform.up.z;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb.AddForce(-transform.right * moveSpeed);
+            rb.AddForce(moveSpeed * Time.fixedDeltaTime * -transform.right);
         }
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            rb.AddForce(transform.right * moveSpeed);
+            rb.AddForce(moveSpeed * Time.fixedDeltaTime * transform.right);
         }
 
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
     }
 }
