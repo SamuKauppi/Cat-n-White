1c88eea337f94789daea74759eea307222372998
Giselle <giselle.altamiranda@outlook.com.ar>
2025-01-25 16:57:04 +0200

    Credits added

--- Assets/Scripts/VisionManager.cs
+++ Assets/Scripts/VisionManager.cs
@@ -6,21 +6,25 @@ public class VisionManager : MonoBehaviour
 {
 	[SerializeField] private BubbleManager bubbleManager;
 	[SerializeField] private PlayerMove player;
+	[SerializeField] private Sprite Hblack;
+	[SerializeField] private Sprite Hwhite;
 	public Color black;
 	public Color white;
 	public float speed = 0.1f;
 
 	private float t;
 	private GameObject bubble;
-	
 
+	private void Start()
+	{
+	}
 	private IEnumerator ShowThings(Color currentColor, Color targetColor)
 	{
 		while (t < speed)
 		{
 			t += Time.deltaTime;
-//			player.GetComponent<SpriteRenderer>().sprite= 
-			bubbleManager.bubbles[0].GetComponent<SpriteRenderer>().color = Color.Lerp(targetColor, currentColor, t / speed);
+			player.GetComponent<SpriteRenderer>().color = black;
+			//bubbleManager.bubbles[0].GetComponent<SpriteRenderer>().color = Color.Lerp(targetColor, currentColor, t / speed);
 			Camera.main.backgroundColor = Color.Lerp(currentColor, targetColor, t / speed);
 			yield return null;
 		}
@@ -30,11 +34,13 @@ public class VisionManager : MonoBehaviour
 	{
 		if (Camera.main.backgroundColor == black)
 		{
+			player.GetComponent<SpriteRenderer>().sprite = Hwhite;
 			t = 0;
 			StartCoroutine(ShowThings(black, white));
 		}
 		else
 		{
+			player.GetComponent<SpriteRenderer>().sprite = Hblack;
 			t = 0;
 			StartCoroutine(ShowThings(white, black));
 		}
